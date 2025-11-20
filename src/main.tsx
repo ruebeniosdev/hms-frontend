73b871ecdb09e499e386289be7fc6ef154ea6972
yaasarkodie <nynsarkodie1919@gmail.com>
2025-11-20 00:51:54 +0000

    commend deleted

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { routeTree } from './routeTree.gen'
 // Create a new router instance
 const router = createRouter({ routeTree })
 
-// Register the router instance for type safety
+
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
