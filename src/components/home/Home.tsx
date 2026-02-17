2eb7b5f32453bb545a244f1b72e58789e3cb83ec
Yaa Sarkodie <nynsarkodie1919@gmail.com>
2026-02-17 01:54:24 +0000

    Added RoleAccess component to Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,6 @@
 import { Hero } from '@/components/Hero/hero';
 import Features from '@/components/auth/Feature/feature';
+import RoleAccess from '@/components/RoleAccess/roleaccess';
 
 export const Home = () => {
   return (
@@ -8,6 +9,7 @@ export const Home = () => {
       <section className="py-12">
         <div className="container mx-auto px-4">
           <Features />
+          <RoleAccess />
         </div>
       </section>
     </div>
